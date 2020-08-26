5483aaecc1afb7026beb2b50cd6b28ce47df97f1
Nicola <nicolavalente88@gmail.com>
2020-08-26 17:09:49 +0200

    Prova 2

--- index.php
+++ index.php
@@ -5,18 +5,40 @@
 Class User {
   public $name;
   public $surname;
-  public $year_of_birth;
   public $nationality;
   public $username;
   public $email;
   public $password;
+
+  //Methods
+
+  //Method birthDate
+  //Arguments year of birth, month of birth, day of birth
+  //Return a string that rapresent the date of birth
+  public function birthDate($year, $month, $day) {
+
+    //Se $day e` un numero inferiore a 10
+    //$day sara` preceduto da uno 0
+    if ($day < 10 ) {
+      $day = 0 . $day;
+    }
+
+    //Se $month e` un numero inferiore a 10
+    //$month sara` preceduto da uno 0
+    if ($month < 10 ) {
+      $month = 0 . $month;
+    }
+
+    $this->birth_date = $year . "-" . $month . "-" . $day;
+    return $this->birth_date;
+  }
 
 }
 
 $pippo = new User();
 $pippo->name = "Pippo";
 $pippo->surname = "Verdi";
-$pippo->year_of_birth = 1989;
+$pippo->birthDate(1989, 7, 1);
 $pippo->nationality = "Italian";
 $pippo->username = "pippo1";
 $pippo->email = "pippo@gmail.com";
